f467eb045a92736721559f64323364b90282aae6
JuliaAlanne <53877127+JuliaAlanne@users.noreply.github.com>
2022-06-24 00:21:56 -0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,9 +1,42 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
+#include "Sculptor.h"
+#include "Interpretadordearquivos.h"
+#include "FiguraGeometrica.h"
+#include "cutBox.h"
+#include "CutEllipsoid.h"
+#include "CutSphere.h"
+#include "cutVoxel.h"
+#include "putBox.h"
+#include "PutEllipsoid.h"
+#include "PutSphere.h"
+#include "putVoxel.h"
 
-using namespace std;
+int main(){
+    Sculptor *s;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+    Interpretadordearquivos interp;
+
+    std::vector<FiguraGeometrica*> fg;
+
+    fg = interp.parse("testeOFF.off");
+
+    s = new Sculptor(interp.getDx(), interp.getDy(), interp.getDz());
+
+    for(size_t i=0; i<fg.size(); i++) {
+        std::cout << "Desenho\n";
+        fg[i]->draw(*s);
+    }
+
+
+    s->writeOFF((char*)"testeOFF.off");
+
+    for(size_t i=0; i<fg.size(); i++) {
+        delete fg[i];
+    }
+
+    delete s;
     return 0;
-}
+
+    }
